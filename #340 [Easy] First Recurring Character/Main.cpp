a2a21dd39b1ddbe1d8613cf01ece350469ea2376
Ben Baginski <benmandude@gmail.com>
2018-03-20 05:17:47 -0600

    #340 [Easy] First Recurring Character

---
+++ #340 [Easy] First Recurring Character/Main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	string s;
+	if (argc == 2){ 
+		s = argv[1]; 
+		cout << "Entered String: " << s << endl;
+	}
+	else{
+		cout << "Enter String: ";
+		cin >> s;
+	}
+	for (unsigned i = 1; i < s.size(); i++)
+	{
+		for (unsigned j = 0; j < i; j++)
+		{
+			cout << "[" << i << "]-" << s[i] << " <-> ";
+			cout << "[" << j << "]-" << s[j] << " " << endl;
+
+			if (s[i] == s[j])
+			{
+				cout << s[i] << " repeats at indexes " << j << " and " << i << endl;
+				return 0;
+			}
+		}
+	}
+	cout << "No character repeats" << endl;
+	return 0;
+}
