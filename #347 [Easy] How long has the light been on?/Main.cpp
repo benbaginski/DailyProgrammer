7455b0d8f15c463dd0948a29180b3061c7ad9415
Ben Baginski <benmandude@gmail.com>
2018-03-20 18:19:47 -0600

    #347 Light On

---
+++ #347 [Easy] How long has the light been on?/Main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main(){
+	
+	map<int, bool> onMap;
+	int in, out;
+	int hoursOn = 0;
+
+	//Add times to map
+	while(cin >> in >> out){
+		for (int i = in; i < out; i++){
+			onMap[i] = true;
+		}
+	}
+
+	//Add hours, print times
+	for (int i = 0; i < onMap.size(); i++){
+		if(onMap[i] == true) hoursOn++;
+		cout << i << "h to " << i+1 << "h " << onMap[i] << endl;
+	}
+
+	cout << "Lights were on for " << hoursOn << " hours." << endl;
+}
